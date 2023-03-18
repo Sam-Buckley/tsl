3d53dffb50c88edf46d64b9e905a291680165694
EjjiCoUk <88962595+EjjiCoUk@users.noreply.github.com>
2023-03-18 20:28:44 +0000

    Create scanner.js

---
+++ src/scanner.js
@@ -0,0 +1,123 @@
+const error = require("./error.js").error;
+function scanProgram(program) {
+    //split the program into lines
+    templabels = {"@l": 1};
+    error_throw = false;
+    program = program.split("\n");
+    usedLabels = {"@l": 1};
+    gotos = {};
+    l = 0;
+    //loop through the program
+    for (i in program) {
+        l += 1;
+        line = program[i];
+        //if the line is a label, add it to the labels dictionary
+        if (line.trim().startsWith(":")) {
+            //get the label name
+            var labelName = line.split(":")[1].trim();
+            //if the label is already in the labels dictionary, throw an error
+            if (labelName in templabels) {
+                error("Label " + labelName + " is defined twice", "NameError", l);
+                error_throw = true;
+            }
+            //add the label to the labels dictionary
+            templabels[labelName] = i;
+        }
+        if(line.trim().startsWith("goto_if")) {
+            //split the by goto, then by spaces, remove empty strings, and get the label name
+            labelName = line.split("goto_if")[1].trim()
+            //check if the label is prefixed with a *
+            if (labelName.startsWith("*")) {
+                labelName = labelName.split("*")[1].trim();
+                gotos[labelName] = l;
+                usedLabels[labelName] = true;
+            } else {
+                error("Label " + labelName + " is referenced without a *", "SyntaxError", l, null, null, hint = "Add a * before the label name", hint_quote = "goto_if *" + labelName);
+            }
+        }
+        //if the line is a call to a label, add it to the used labels list
+        else if (line.trim().startsWith("goto")) {
+            //split the by goto, then by spaces, remove empty strings, and get the label name
+            labelName = line.split("goto")[1].trim()
+            //check if the label is prefixed with a *
+            if (labelName.startsWith("*")) {
+                labelName = labelName.split("*")[1].trim();
+                gotos[labelName] = l;
+                usedLabels[labelName] = true;
+            } else {
+                error("Label " + labelName + " is referenced without a *", "SyntaxError", l, null, null, hint = "Add a * before the label name", hint_quote = "goto *" + labelName);
+            }
+        } 
+        if (line.trim().startsWith("label")) {
+            //split the by goto, then by spaces, remove empty strings, and get the label name
+            labelName = line.split("label")[1].trim()
+            //check if the label is prefixed with a *
+            if (labelName.startsWith("*")) {
+                labelName = labelName.split("*")[1].trim();
+                usedLabels[labelName] = true;
+            } else {
+                error("Label " + labelName + " is referenced without a *", "SyntaxError", l, null, null, hint = "Add a * before the label name", hint_quote = "label *" + labelName);
+            }
+        }
+        //check if the line references a label without a *
+        //iterate through the labels dictionary and check if the line contains the label name
+        for (label in templabels) {
+            if (line.startsWith(label)) {
+                error("Label " + label + " is referenced without a *", "SyntaxError", l, null, null, hint = "Add a * before the label name", hint_quote = "*" + label);
+                error_throw = true;
+            }
+        }
+    }
+    main = false;
+    exit = false
+    for (i in program) {
+        line = program[i];
+        if (line.trim().startsWith(":main")) {
+            main = true;
+        } else if (line.trim().startsWith("exit")) {
+            exit = true;
+        }
+    }
+    if (main != true) {
+        error("No main label found", "NameError", 0, null, null, hint = "Add a main label to your program", hint_quote = ":main");
+        error_throw = true;
+    }
+    if (exit != true) {
+        error("No exit statement found", "NameError", 0, null, null, hint = "Add an exit statement to your program", hint_quote = "exit");
+        error_throw = true;
+    }
+    //iterate through the labels dictionary and check if the label is used, if not, throw an error
+    for (label in templabels) {
+        if(label == "main") {
+            continue;
+        }
+        if (!(label in usedLabels)) {
+            error("Label " + label + " is defined but never used", "Dead Code", templabels[label], `:${label}`, `:${label}`, hint = "Remove the label", "");
+            error_throw = true;
+        }
+    }
+    //iterate through the used labels list and check if the label is defined, if not, throw an error
+    for (label in usedLabels) {
+        if (!(label in templabels)) {
+            error("Label " + label + " is used but never defined", "NameError", gotos[label] - 1, `goto *${label}`, `goto *${label}`, hint = "Define the label", hint_quote = `:${label}`);
+            error_throw = true;
+        }
+    }
+    //iterate through the program and check if there are unclosed quotes
+    for (i in program) {
+        line = program[i];
+        if (line.split('"').length % 2 == 0) {
+            error("Unclosed quotes", "SyntaxError", i, line.trim(), `"`, hint = "Close the quotes", hint_quote = `${line.trim()}"`);
+            error_throw = true;
+        }
+    }
+    return error_throw;
+}
+//create a function to determine if each label is reached in the program or if it's skipped
+function reachable(program) {
+    //split the program into lines
+    program = program.split("\n");
+    //store labels above main
+}
+
+module.exports = scanProgram;
